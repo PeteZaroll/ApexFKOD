e265e6d8e658fd3a25065293413c7707c1d6a57d
Alex D <Amd134@pitt.edu>
2019-04-15 19:26:55 -0400

    initial upload

---
+++ scripts.js
@@ -0,0 +1,67 @@
+//Legend Vars
+var lifeline = document.getElementById("lifeline");
+var bangalore = document.getElementById("bangalore");
+var octane = document.getElementById("octane");
+var wraith = document.getElementById("wraith");
+var mirage = document.getElementById("mirage");
+var bloodhound = document.getElementById("bloodhound");
+var pathfinder = document.getElementById("pathfinder");
+var gibraltar = document.getElementById("gibraltar");
+var caustic = document.getElementById("caustic");
+
+//Button Vars
+var lifelineButton = document.getElementById('lifelineButton');
+var bangaloreButton = document.getElementById('bangaloreButton');
+var octaneButton = document.getElementById('octaneButton');
+var wraithButton = document.getElementById('wraithButton');
+var mirageButton = document.getElementById('mirageButton');
+var bloodhoundButton = document.getElementById('bloodhoundButton');
+var pathfinderButton = document.getElementById('pathfinderButton');
+var gibraltarButton = document.getElementById('gibraltarButton');
+var causticButton = document.getElementById('causticButton');
+
+
+function changeColorLifeline() {
+  lifeline.style.borderColor="green";
+  lifelineButton.style.background="green";
+}
+
+function changeColorBangalore() {
+  bangalore.style.borderColor="green";
+  bangaloreButton.style.background="green";
+}
+
+function changeColorOctane() {
+  octane.style.borderColor="green";
+  octaneButton.style.background="green";
+}
+
+function changeColorWraith() {
+  wraith.style.borderColor="green";
+  wraithButton.style.background="green";
+}
+
+function changeColorMirage() {
+  mirage.style.borderColor="green";
+  mirageButton.style.background="green";
+}
+
+function changeColorBloodhound() {
+  bloodhound.style.borderColor="green";
+  bloodhoundButton.style.background="green";
+}
+
+function changeColorPathfinder() {
+  pathfinder.style.borderColor="green";
+  pathfinderButton.style.background="green";
+}
+
+function changeColorGibraltar() {
+  gibraltar.style.borderColor="green";
+  gibraltarButton.style.background="green";
+}
+
+function changeColorCaustic() {
+  caustic.style.borderColor="green";
+  causticButton.style.background="green";
+}
